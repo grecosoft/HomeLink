7a11a31ef4cf3c7ba85abb843175170b5028cf29
Brian Greco <blgreco72dev@gmail.com>
2024-04-12 20:26:57 -0400

    Added missing registration code.

--- microservices/HomeLink.Management/src/Components/HomeLink.Management.Infra/Plugin/InfraPlugin.cs
+++ microservices/HomeLink.Management/src/Components/HomeLink.Management.Infra/Plugin/InfraPlugin.cs
@@ -8,9 +8,12 @@ public class InfraPlugin : PluginBase
     public override string PluginId => "74DD6A00-FD99-4C21-84D1-A22A6057D53B";
     public override PluginTypes PluginType => PluginTypes.AppPlugin;
     public override string Name => "Infrastructure Application Component";
-
+    
     public InfraPlugin() {
+        AddModule<DigitalTwinModule>();
         AddModule<RepositoryModule>();
+        AddModule<IotHubModule>();
+        AddModule<IotCentralModule>();
 
         Description = "Plugin component containing the application infrastructure.";
     }
